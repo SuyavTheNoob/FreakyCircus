083cfb1d18e7f863a4d47f9af760f29af204da5b
SuyavTheNoob <160936754+SuyavTheNoob@users.noreply.github.com>
2024-07-14 16:44:13 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+
+
